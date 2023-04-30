5de49fb6a698c2309836d998565f0bcf09ec9f5c
Eason <eason.liao.1994@gmail.com>
2023-04-30 11:38:40 +0800

    Refactor TextField border style with custom ViewModifier

--- SwiftUI-Focused-TextField/ContentView.swift
+++ SwiftUI-Focused-TextField/ContentView.swift
@@ -7,53 +7,44 @@
 
 import SwiftUI
 
+struct FocusedTextField: View {
+    
+    @FocusState private var isFocused: Bool
+    @Binding var text: String
+    let placeholder: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.headline)
+                Spacer()
+            }
+            
+            TextField(placeholder, text: $text)
+                .focused($isFocused)
+                .onChange(of: isFocused) { focused in
+                    isFocused = focused
+                }
+                .frame(height: 30)
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(isFocused ? Color.yellow : Color.gray, lineWidth: 1)
+                )
+        }
+    }
+}
+
 struct ContentView: View {
-    
-    @FocusState private var isFocusedName
-    @FocusState private var isFocusedDescription
-    
     @State var name: String = ""
     @State var description: String = ""
     
     var body: some View {
         VStack {
-            HStack {
-                Text("Name")
-                    .font(.headline)
-                
-                Spacer()
-            }
-            
-            TextField("Input your name", text: $name)
-                .focused($isFocusedName)
-                .onChange(of: isFocusedName) { focused in
-                    isFocusedName = focused
-                }
-                .frame(height: 30)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(isFocusedName ? Color.yellow : Color.gray, lineWidth: 1)
-                )
-            HStack {
-                Text("Description")
-                    .font(.headline)
-                
-                Spacer()
-            }
-            
-            TextField("Input your description", text: $description)
-                .focused($isFocusedDescription)
-                .onChange(of: isFocusedDescription) { focused in
-                    isFocusedDescription = focused
-                }
-                .frame(height: 30)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(isFocusedDescription ? Color.yellow : Color.gray, lineWidth: 1)
-                )
-                
+            FocusedTextField(text: $name, placeholder: "Input your name", title: "Name")
+            FocusedTextField(text: $description, placeholder: "Input your description", title: "Description")
         }
         .padding()
     }
